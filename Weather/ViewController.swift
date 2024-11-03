20e1ee6aaafc9fd90fb35eda01f52c94f50146a0
NguyenLeHieu <nguyenhieu157380@gmail.com>
2024-11-04 00:39:29 +0700

    Added: Real time background change

--- Weather/ViewController.swift
+++ Weather/ViewController.swift
@@ -67,6 +67,7 @@ extension ViewController {
                 let weatherData = try JSONDecoder().decode(WeatherModel.self, from: data)
                 DispatchQueue.main.async {
                     self.displayWeather(weatherData)
+                    self.changeBackground(weatherData.dt, weatherData.sys.sunrise, weatherData.sys.sunset)
                 }
             } catch {
                 print("Error handle \(error)")
@@ -144,6 +145,57 @@ extension ViewController {
         formatter.dateFormat = "HH:mm"
         let formatterHour = formatter.string(from: date)
         return formatterHour
+    }
+    
+    // real time background change
+    func changeBackground(
+        _ datetime: Int,
+        _ sunrise: Int,
+        _ sunset: Int
+    ){
+        switch datetime {
+            
+        case sunrise..<(sunrise + 1800), (sunset - 1800)...sunset:
+            // Gradient color sunrise and sunset
+            setGradientBackground(color: [
+                UIColor(red: 4.0/255.0, green: 64.0/255.0, blue: 204.0/255.0, alpha: 1.0).cgColor, // màu xanh nước biển
+                UIColor(red: 179.0/255.0, green: 149.0/255.0, blue: 220.0/255.0, alpha: 1.0).cgColor, // màu tím nhạt
+                UIColor(red: 244.0/255.0, green: 174.0/255.0, blue: 94.0/255.0, alpha: 1.0).cgColor, // màu vàng
+                UIColor(red: 175.0/255.0, green: 61.0/255.0, blue: 54.0/255.0, alpha: 1.0).cgColor // màu cam
+            ], location: [0.0, 0.5, 0.88, 1.0])
+            
+        case (sunrise + 1800)..<(sunset - 1800):
+            // Gradient color day
+            setGradientBackground(color: [
+                UIColor(red: 135.0/255.0, green: 206.0/255.0, blue: 235.0/255.0, alpha: 1.0).cgColor, // Xanh trời nhạt
+                UIColor(red: 255.0/255.0, green: 223.0/255.0, blue: 186.0/255.0, alpha: 1.0).cgColor, // Vàng sáng
+                UIColor(red: 70.0/255.0, green: 130.0/255.0, blue: 180.0/255.0, alpha: 1.0).cgColor // Xanh da trời
+            ], location: [0.0, 0.5 , 1.0])
+            
+        default:
+            // Gradient color night
+            setGradientBackground(color: [
+                UIColor(red: 11.0/255.0, green: 61.0/255.0, blue: 145.0/255.0, alpha: 1.0).cgColor, // Xanh đậm
+                UIColor(red: 44.0/255.0, green: 22.0/255.0, blue: 84.0/255.0, alpha: 1.0).cgColor, // Tím đậm
+                UIColor(red: 18.0/255.0, green: 32.0/255.0, blue: 47.0/255.0, alpha: 1.0).cgColor // Màu đêm tối
+            ], location: [0.0, 0.5 , 1.0])
+        }
+    }
+    
+    // Helper function to set the gradient background
+    func setGradientBackground(color: [CGColor], location: [NSNumber]) {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = color
+        gradientLayer.locations = location
+        //set the start and end point for the gradient
+        gradientLayer.startPoint = CGPoint(x: 0, y: 0)
+        gradientLayer.endPoint = CGPoint(x: 0, y: 1.0)
+        
+        //set the frame tho the layer
+        gradientLayer.frame = view.frame
+        
+        //add the gradient layer as as sublayer to the background view
+        view.layer.insertSublayer(gradientLayer, at: 0)
     }
 }
 
